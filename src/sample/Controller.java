8cc3e38558ca5dc3cc7ac675318338957ced83d6
fardisaz <amoozadeh73@yahoo.com>
2020-09-09 14:07:12 +0200

    override stop & init in Main

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,14 @@ package sample;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 import sample.datamodel.Plan;
 import sample.datamodel.PlanData;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
@@ -27,35 +25,29 @@ public class Controller {
     @FXML
     Label dateLabel;
 
-    public void initialize(){
-        Plan plan1=new Plan("Personal Photo","Taking a personal photo for job application",
-                LocalDate.of(2020, Month.OCTOBER,11));
-        Plan plan2=new Plan("Birthday Present","Mathew's birthday is on 23rd of October",
-                LocalDate.of(2020,Month.OCTOBER,22));
-        Plan plan3=new Plan("Frankfurt Trip","Renew my passport on Tuesday",
-                LocalDate.of(2020,Month.OCTOBER,15));
-        Plan plan4=new Plan("Master Thesis","I have a meeting with my supervisor",
-                LocalDate.of(2020,Month.SEPTEMBER,11));
-        Plan plan5=new Plan("Visit Köln","Meet my previous co-worker",
-                LocalDate.of(2020,Month.NOVEMBER,28));
-        Plan plan6=new Plan("Christmas Presents","Buying presents for my friends",
-                LocalDate.of(2020,12,10));
-
-        //plans data
-        plans=new ArrayList<>();
-        plans.add(plan1);
-        plans.add(plan2);
-        plans.add(plan3);
-        plans.add(plan4);
-        plans.add(plan5);
-        plans.add(plan6);
-
-        PlanData.getInstance().setPlans(plans);
-        try {
-            PlanData.getInstance().storePlan();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+       public void initialize(){
+//        Plan plan1=new Plan("Personal Photo","Taking a personal photo for job application",
+//                LocalDate.of(2020, Month.OCTOBER,11));
+//        Plan plan2=new Plan("Birthday Present","Mathew's birthday is on 23rd of October",
+//                LocalDate.of(2020,Month.OCTOBER,22));
+//        Plan plan3=new Plan("Frankfurt Trip","Renew my passport on Tuesday",
+//                LocalDate.of(2020,Month.OCTOBER,15));
+//        Plan plan4=new Plan("Master Thesis","I have a meeting with my supervisor",
+//                LocalDate.of(2020,Month.SEPTEMBER,11));
+//        Plan plan5=new Plan("Visit Köln","Meet my previous co-worker",
+//                LocalDate.of(2020,Month.NOVEMBER,28));
+//        Plan plan6=new Plan("Christmas Presents","Buying presents for my friends",
+//                LocalDate.of(2020,12,10));
+//        plans=new ArrayList<Plan>();
+//
+//        plans.add(plan1);
+//        plans.add(plan2);
+//        plans.add(plan3);
+//        plans.add(plan4);
+//        plans.add(plan5);
+//        plans.add(plan6);
+//
+//        PlanData.getInstance().setPlans(plans);
 
         //eventListener
         PlansList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Plan>() {
@@ -69,7 +61,7 @@ public class Controller {
                 }
             }
         });
-        PlansList.getItems().setAll(plans);
+        PlansList.getItems().setAll(PlanData.getInstance().getPlans());
         PlansList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         PlansList.getSelectionModel().selectFirst();
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.datamodel.PlanData;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -19,5 +22,23 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try{
+            PlanData.getInstance().storePlan();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try{
+            PlanData.getInstance().loadPlans();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
